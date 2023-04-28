9fcc58e1503c110d02f9ccb5787b77ba5bcafb18
Kay Cee <se.kaycee@gmail.com>
2023-04-28 14:38:25 +0100

    update: Delete a State object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ View for State objects that handles default API actions """
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, make_response
 from models import storage
 from models.state import State
 
@@ -19,3 +19,15 @@ def get_state_id(state_id):
     if not state:
         abort(404)
     return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_state(state_id):
+    """ Delete a State object """
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    state.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
