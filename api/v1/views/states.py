0347c47964e75e1cd6188730973f1ea10e45088a
Kay Cee <se.kaycee@gmail.com>
2023-04-28 14:34:53 +0100

    update: Retrieve a State object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ View for State objects that handles default API actions """
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 from models.state import State
 
@@ -10,3 +10,12 @@ from models.state import State
 def states():
     """ Retrieve the list of all State objects """
     return jsonify([obj.to_dict() for obj in storage.all(State).values()])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state_id(state_id):
+    """ Retrieve a State object """
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
