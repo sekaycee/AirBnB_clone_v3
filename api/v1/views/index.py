5edfc44946930c65fbd8115b0fa7c77393c1e0ff
Kay Cee <se.kaycee@gmail.com>
2023-04-28 13:38:54 +0100

    update: get the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,20 @@
 """ Index view """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Returns JSON """
+    """ get http status as JSON """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ get the number of each instance type as JSON """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
